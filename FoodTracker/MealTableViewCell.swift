f4d40facf4b0fcfac4ffeb150bae45fd0b9934d6
Matt Light <matt@paren.io>
2018-12-28 22:27:27 -0800

    Step 6: Create a Table View

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Matt Light on 12/28/18.
+//  Copyright © 2018 Matt Light. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
